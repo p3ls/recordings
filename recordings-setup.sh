3c782ebbbe1ea6b8b590e839bea8982222cb1734
stef <dev@konsequence.net>
2012-09-19 20:00:46 +0200

    setup script changes

--- recordings-setup.sh
+++ recordings-setup.sh
@@ -2,13 +2,13 @@
 
 if [ -z $1 ] ; then echo 'pass a conf file as argument. exit.'; exit 0; fi
 
-echo "contents of config file: $1"
+# echo "contents of config file: $1"
 
 title=''
 base_dir=''
-stream_dir=''
-cover_dir=''
-log_dir=''
+stream_dir='audio'
+cover_dir='img'
+log_dir='log'
 stream_format=''
 stream_url=''
 cover_url=''
@@ -17,19 +17,25 @@ station_url=''
 metadata_script=''
 length_in_minutes=''
 
+mkdir "$base_dir/$stream_dir" "$base_dir/$log_dir"
+
+# iterating lines in config file.
+
 while read i ; do
  
   j=$(echo $i|cut -d'=' -f1)
-#echo $j
+ 
+ #echo $j
 
+ # found title in config file
+ # create lower case project folder
+ # create upper case station title
  if [ "$j" = "title" ] ; then
   base_dir=$(echo $i|cut -d'=' -f2|sed 's/ /-/g'| tr '[:upper:]' '[:lower:]')
   title=$(echo $i|cut -d'=' -f2|sed 's/-/ /g'| tr '[:lower:]' '[:upper:]')
   echo "creating directory... $base_dir"
-  mkdir $base_dir
-  
+  mkdir $base_dir 
  fi
-
 
 
  if [ "$j" = "stream_url" ] ; then
@@ -37,30 +43,27 @@ while read i ; do
   echo "testing stream... $url"
   streamripper $stream_url --quiet -d $base_dir -a "test" -A -l 3
 
-
   if [ -e "$base_dir/test.aac" ] ; then
-   echo "got aac file."
+   echo "stream is in aac format."
    stream_format='aac'
-   stream_dir='aac'
+   rm "$base_dir/test.aac" 
   fi
 
   if [ -e "$base_dir/test.mp3" ] ; then
-   echo "got mp3 file."
+   echo "stream is in mp3 format."
    stream_format='mp3'
-   stream_dir='mp3'
+   rm "$base_dir/test.mp3"
   fi
 
-  rm "$base_dir/test.aac" "$base_dir/test.cue" "$base_dir/test.mp3"
+  rm "$base_dir/test.cue"
 
-  mkdir "$base_dir/$stream_dir"
-
+  
  fi
 
  if [ "$j" = "cover_url" ] ; then
   cover_url=$(echo $i|cut -d'=' -f2)
   format=$(echo $cover_url|cut -d'.' -f4)
   echo "cover format: $format"
-  cover_dir=$format
   mkdir "$base_dir/$cover_dir"
   cover_path="$base_dir/$cover_dir/null.$format"
   echo "downloading cover to... $cover_path"
@@ -86,8 +89,7 @@ while read i ; do
 
 done < $1
 
-log_dir="log"
-mkdir "$base_dir/$log_dir"
+
 
 
 echo 'summary:'
@@ -105,6 +107,7 @@ echo "station_url     : $station_url"
 echo ''
 
 
+echo 'creating extended config file. is later used by other scripts.'
 
 echo "title=$title" > "$base_dir.conf"
 echo "base_dir=$base_dir" >> "$base_dir.conf"
