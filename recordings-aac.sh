afb64175e37571902c8d6caf967704c1a4a2d299
stef <dev@konsequence.net>
2012-09-20 01:19:24 +0200

    setup

--- recordings-aac.sh
+++ recordings-aac.sh
@@ -8,15 +8,13 @@ if [ -z $1 ] ; then echo 'pass a conf file as argument. exit.'; exit 0; fi
 echo "contents of config file: $1"
 
 
-name='P1 REVERSE'
-url='http://www.paris-one.com/pls/radio_v6.m3u'
-length_in_min=60
-expected_kbs=128
-metafile='' # comes through stream in cue sheet recordings-metadata.sh'
-log='log' # dep
-logdir='log'
-playlist='log/playlist.csv' # dep
-playlist_file="log/playlist-file.csv" # dep
+#name='P1 REVERSE'
+#url='http://www.paris-one.com/pls/radio_v6.m3u'
+#length_in_min=60
+#expected_kbs=128
+#metafile='' # comes through stream in cue sheet recordings-metadata.sh'
+#log='log' # dep
+#logdir='log'
 coverdir="jpg/final_covers"
 cover_image="jpg/album_covers/null.jpg" #dep use front_cover
 front_cover="jpg/album_covers/null.jpg"
@@ -41,6 +39,10 @@ station_url=''
 metadata_script=''
 length_in_minutes=''
 
+playlist="$log_dir/playlist.csv" # dep
+playlist_file="$log_dir/playlist-file.csv" # dep
+
+
 # iterating lines in config file.
 
 while read i ; do
@@ -52,9 +54,14 @@ while read i ; do
  # found title in config file
  # create lower case project folder
  # create upper case station title
+
+ if [ "$j" = "stream_format" ] ; then
+  stream_format=$(echo $i|cut -d'=' -f2)
+ fi
+
+
  if [ "$j" = "title" ] ; then
-  base_dir=$(echo $i|cut -d'=' -f2|sed 's/ /-/g'| tr '[:upper:]' '[:lower:]')
-  title=$(echo $i|cut -d'=' -f2|sed 's/-/ /g'| tr '[:lower:]' '[:upper:]')
+  title=$(echo $i|cut -d'=' -f2)
  fi
 
 
@@ -67,11 +74,20 @@ while read i ; do
  fi
 
 
- if [ "$j" = "cover_url" ] ; then
-  cover_url=$(echo $i|cut -d'=' -f2)
-  format=$(echo $cover_url|cut -d'.' -f4)  
-  cover_path="$base_dir/$cover_dir/null.$format"
+ if [ "$j" = "stream_dir" ] ; then
+  stream_dir=$(echo $i|cut -d'=' -f2)
  fi
+
+
+ if [ "$j" = "cover_dir" ] ; then
+  cover_dir=$(echo $i|cut -d'=' -f2)
+ fi
+
+
+ if [ "$j" = "log_dir" ] ; then
+  log_dir=$(echo $i|cut -d'=' -f2)
+ fi
+
 
  if [ "$j" = "station_url" ] ; then
   station_url=$(echo $i|cut -d'=' -f2)
@@ -86,7 +102,9 @@ while read i ; do
   length_in_minutes=$(echo $i|cut -d'=' -f2)
  fi
 
-
+ if [ "$j" = "cover_path" ] ; then
+  cover_path=$(echo $i|cut -d'=' -f2)
+ fi
 done < $1
 
 
@@ -98,20 +116,22 @@ done < $1
 
 echo 'summary:'
 echo ''
-echo "base_dir        : $base_dir"
-echo "stream_dir      : $base_dir/$stream_dir"
-echo "cover_dir       : $base_dir/$cover_dir"
-echo "log_dir         : $base_dir/$log_dir"
-echo "stream_format   : $stream_format"
-echo "stream_url      : $stream_url"
-echo "cover_url       : $cover_url"
-echo "cover_path      : $cover_path"
-echo "station_url     : $station_url"
+echo "title             : $title"
+echo "stream_dir        : $stream_dir"
+echo "cover_dir         : $cover_dir"
+echo "log_dir           : $log_dir"
+echo "stream_format     : $stream_format"
+echo "stream_url        : $stream_url"
+echo "cover_path        : $cover_path"
+echo "station_url       : $station_url"
+echo "length_in_minutes : $length_in_minutes"
+echo "metadata_script   : $metadata_script"
+
 
 echo ''
 
 
-exit 0
+
 
 
 
@@ -133,24 +153,25 @@ exit 0
 ###############################################################
 u='UTF-8'
 # 10 = debug 
-ed=$(($length_in_min*60)) # expected duration in seconds
+ed=$(($length_in_minutes*60)) # expected duration in seconds
 fd=""			  # final duration of file (measured)
-cut="" # dep, use mp3cut_str 
+cut=""            # dep, use mp3cut_str 
 mp3cut_str=""
 
-if [ ! -e "$type/" ] ; then mkdir $type; fi
+#if [ ! -e "$type/" ] ; then mkdir $type; fi
 
 echo 'cleaning up previous temp files...'
 echo "delete everything in $logdir" 
-for file in $(find $logdir -name '*.*') ; do
+for file in $(find $log_dir -name '*.*') ; do
  rm -Rif $file
 done  
 
 echo 'cleaning up cue sheets...'
-for file in $(find $mp3dir -name '*.cue') ; do
+for file in $(find $stream_dir -name '*.cue') ; do
  rm -f $file
 done  
 
+exit 0
 
 for i in 1 2 3 ; do
  ts=$(date -u "+%s")	# now - universal time
@@ -165,15 +186,15 @@ for i in 1 2 3 ; do
  filename2=$(echo $filename|sed 's/ /_/g')
  f3='' # deprec. 
  filename3='' # used for generated part files filename (1).mp3
- playlist="$logdir/$filename2.playlist"
- playlist2="$logdir/$filename2.playlist2"
+ playlist="$log_dir/$filename2.playlist"
+ playlist2="$log_dir/$filename2.playlist2"
  echo "start recording         : #$i $name"
  echo "time                    : $ta ($tsn)"
  echo "expected end            : $tb"
- echo "expected length         : $length_in_min min"
+ echo "expected length         : $length_in_minutes min"
  #echo "writing playlist to     : $playlist2" 
 #./recordings-metadata.sh $filename2 &
- echo "writing stream to       : $mp3dir/$filename2.mp3"
+ echo "writing stream to       : $stream_dir/$filename2.$stream_format"
 #streamripper $url --quiet -d $destination_dir -a $filename2 -A -l $ed --codeset-metadata=$u --codeset-filesys=$u --codeset-id3=$u --xs-offset=500
 
 #       -A             - Don't write individual tracks
@@ -204,15 +225,15 @@ for i in 1 2 3 ; do
 
 
 
-
+break;
 
 #iconv --from-code=ISO-8859-1 --to-code=UTF-8 BASSTUNE_2012-09-11_00H38_TO_00H48.cue > cue-utf.txt
 
-streamripper $url -d $mp3dir -a $filename2 -A -l $ed --codeset-filesys=UTF-8 --codeset-id3=ISO-8859-15 --codeset-metadata=ISO-8859-15 --codeset-relay=ISO-8859-15 # --codeset-metadata=$u --codeset-filesys=$u --codeset-id3=$u
+streamripper $stream_url -d $stream_dir -a $filename2 -A -l $ed --codeset-filesys=UTF-8 --codeset-id3=ISO-8859-15 --codeset-metadata=ISO-8859-15 --codeset-relay=ISO-8859-15 # --codeset-metadata=$u --codeset-filesys=$u --codeset-id3=$u
 
  echo "convert cuesheet into textfile"
- cuesheet=$(cat "$mp3dir/$filename2.cue") 
- printf "%s\n" "$cuesheet" | tr '[:lower:]' '[:upper:]' > "$mp3dir/$filename2.txt"
+ cuesheet=$(cat "$stream_dir/$filename2.cue") 
+ printf "%s\n" "$cuesheet" | tr '[:lower:]' '[:upper:]' > "$stream_dir/$filename2.txt"
 
  #m=$(echo $m |tr '[:lower:]' '[:upper:]') # make all lowercase chars upper in month name
 
@@ -221,9 +242,18 @@ streamripper $url -d $mp3dir -a $filename2 -A -l $ed --codeset-filesys=UTF-8 --c
  echo "cleaning up..."
  echo "searching part files..."
  
+ 
+ if [ "$stream_format" = "aac" ] ; then
+ 
+  
+ 
+ 
+ 
+ else if [ "$stream_format" = "mp3" ] ; then
+
  for j in 1 2 3 4 5 6 ; do		 # - number of part files
   b="$f2 ($j)"
-  c="$mp3dir/$b.mp3"
+  c="$stream_dir/$b.mp3"
   echo "searching for... $c"
    if [ -e "$c" ]; then
     echo "part file found... $c"
@@ -239,21 +269,21 @@ streamripper $url -d $mp3dir -a $filename2 -A -l $ed --codeset-filesys=UTF-8 --c
  if [ "$cut" = "" ]; then
   echo "no parts found."
  else
-  cut="-o $mp3dir/$filename2.join.mp3 $cut -t 00:00 $mp3dir/$filename2.mp3" # add the original file, as last part for joining
-  echo "join part files together in... $mp3dir/$filename2.mp3"  
+  cut="-o $stream_dir/$filename2.join.mp3 $cut -t 00:00 $stream_dir/$filename2.mp3" # add the original file, as last part for joining
+  echo "join part files together in... $stream_dir/$filename2.mp3"  
   echo "final mp3cut string:"
   echo "$cut"
   mp3cut $cut
   sleep 1
   cut="" # reset
-  rm -f "$destdir/$filename2-(1).mp3"
-  rm -f "$destdir/$filename2-(2).mp3"
-  rm -f "$destdir/$filename2-(3).mp3"
-  rm -f "$destdir/$filename2-(4).mp3"
-  rm -f "$destdir/$filename2-(5).mp3"
-  rm -f "$destdir/$filename2-(6).mp3"
-  rm -f "$destdir/$filename2.mp3"
-  mv "$destdir/$filename2.join.mp3" "$destdir/$filename2.mp3"
+  rm -f "$stream_dir/$filename2-(1).mp3"
+  rm -f "$stream_dir/$filename2-(2).mp3"
+  rm -f "$stream_dir/$filename2-(3).mp3"
+  rm -f "$stream_dir/$filename2-(4).mp3"
+  rm -f "$stream_dir/$filename2-(5).mp3"
+  rm -f "$stream_dir/$filename2-(6).mp3"
+  rm -f "$stream_dir/$filename2.mp3"
+  mv "$stream_dir/$filename2.join.mp3" "$stream_dir/$filename2.mp3"
 
  fi # if needs to join
   echo "adding id3 information to it..."
@@ -285,6 +315,12 @@ streamripper $url -d $mp3dir -a $filename2 -A -l $ed --codeset-filesys=UTF-8 --c
  eyeD3  -Y "$id3_year" -A "$id3_album" -t "$id3_title" -a "$id3_artist" --remove-all --add-image=$cover_image:FRONT_COVER: "$destdir/$filename2.mp3" 
 
  echo "tags set."
+
+
+ 
+ fi
+ 
+
 
  echo 'generating index.html...'
 
